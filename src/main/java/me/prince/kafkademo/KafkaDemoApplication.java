315ec8160abdfa94d7a4f3a8b216954fc27e388a
yhjhoo <yhjhoo@gmail.com>
2022-10-05 18:34:09 +0800

    Polish code

--- src/main/java/me/prince/kafkademo/KafkaDemoApplication.java
+++ src/main/java/me/prince/kafkademo/KafkaDemoApplication.java
@@ -3,6 +3,7 @@ package me.prince.kafkademo;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.config.TopicBuilder;
@@ -10,6 +11,9 @@ import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 
 @SpringBootApplication
 @EnableKafka
@@ -31,8 +35,22 @@ public class KafkaDemoApplication {
 
 @Service
 class Listener {
-    @KafkaListener(topics = "quickstart-events1", groupId = "default")
-    void listen(String data) {
-        System.out.println(data);
+    @KafkaListener(topics = "quickstart-events1", groupId = "default", clientIdPrefix = "c1")
+    void listen(
+            @Payload String data,
+            @Header(KafkaHeaders.RECEIVED_PARTITION_ID) List<Integer> partitions,
+            @Header(KafkaHeaders.RECEIVED_TOPIC) List<String> topics,
+            @Header(KafkaHeaders.OFFSET) List<Long> offsets
+    ) {
+        System.out.println("");
+        System.out.println("=================");
+
+
+        System.out.format("partition: %s, topic: %s, offset: %s \n",
+                partitions,
+                topics,
+                offsets
+        );
+        System.out.println("data:" + data);
     }
 }
