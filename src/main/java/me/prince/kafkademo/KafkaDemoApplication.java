ab09bb4de3773f9f0a913611b7ea6f1bbc161ecc
yhjhoo <yhjhoo@gmail.com>
2022-10-05 09:09:16 +0800

    Add topic build to create topic

--- src/main/java/me/prince/kafkademo/KafkaDemoApplication.java
+++ src/main/java/me/prince/kafkademo/KafkaDemoApplication.java
@@ -1,9 +1,14 @@
 package me.prince.kafkademo;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 @SpringBootApplication
@@ -14,6 +19,14 @@ public class KafkaDemoApplication {
         SpringApplication.run(KafkaDemoApplication.class, args);
     }
 
+    @Bean
+    public NewTopic topic() {
+        return TopicBuilder.name("quickstart-events1")
+                .partitions(2)
+                .replicas(3)
+                .compact()
+                .build();
+    }
 }
 
 @Service
