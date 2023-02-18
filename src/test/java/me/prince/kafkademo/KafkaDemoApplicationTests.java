5db9c1c3a9c3a699a6ab755bbd6a7d5f7962f596
yhjhoo <yhjhoo@gmail.com>
2023-02-18 21:56:00 +0800

    fix compile

--- src/test/java/me/prince/kafkademo/KafkaDemoApplicationTests.java
+++ src/test/java/me/prince/kafkademo/KafkaDemoApplicationTests.java
@@ -1,22 +1,13 @@
 package me.prince.kafkademo;
 
-import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.Topology;
-import org.apache.kafka.streams.kstream.KStream;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.filter.TypeExcludeFilters;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.kafka.core.KafkaTemplate;
 
 import java.time.LocalDateTime;
-import java.util.Properties;
 
 @SpringBootTest
-@MockBean({Listener.class})
 class KafkaDemoApplicationTests {
 
     @Test
@@ -53,9 +44,5 @@ class KafkaDemoApplicationTests {
         }
     }
 
-//    @Test
-//    void read() {
-//        kafkaTemplate.
-//    }
 
 }
