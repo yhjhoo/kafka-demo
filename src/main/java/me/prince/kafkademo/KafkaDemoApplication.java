d3c549a8fe4c34107bc0772983e3f379ef273f8e
yhjhoo <yhjhoo@gmail.com>
2023-02-18 21:51:23 +0800

    aws kafka

--- src/main/java/me/prince/kafkademo/KafkaDemoApplication.java
+++ src/main/java/me/prince/kafkademo/KafkaDemoApplication.java
@@ -1,15 +1,14 @@
 package me.prince.kafkademo;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.handler.annotation.Header;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaTemplate;
 
 
 @SpringBootApplication
@@ -20,63 +19,24 @@ public class KafkaDemoApplication {
         SpringApplication.run(KafkaDemoApplication.class, args);
     }
 
-   /* @Bean
+    @Bean
     public NewTopic topic() {
-        return TopicBuilder.name("quickstart-events3")
-                .partitions(20)
-                .replicas(5)
+        return TopicBuilder.name("topic1")
+                .partitions(10)
+                .replicas(1)
                 .compact()
                 .build();
-    }*/
+    }
 
-    /*@Bean
-    CommandLineRunner commandLineRunner(KafkaAdmin kafkaAdmin) {
+    @KafkaListener(id = "myId", topics = "topic1")
+    public void listen(String in) {
+        System.out.println(in);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(KafkaTemplate<String, String> template) {
         return args -> {
-            System.out.println("111");
-            AdminClient adminClient = AdminClient.create(kafkaAdmin.getConfigurationProperties());
-            DeleteTopicsResult result = adminClient.deleteTopics(Arrays.asList("quickstart-events6"));
-            System.out.println(result);
-            adminClient.close();
+            template.send("topic1", "test");
         };
-    }*/
-}
-
-/*@Configuration
-class KafkaTopicConfig {
-
-    @Value(value = "${spring.kafka.bootstrap-servers}")
-    private String bootstrapAddress;
-
-    @Bean
-    public KafkaAdmin kafkaAdmin() {
-        Map<String, Object> configs = new HashMap<>();
-        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        return new KafkaAdmin(configs);
-    }
-
-    @Bean
-    public NewTopic topic1() {
-        return new NewTopic("quickstart-events6", 1, (short) 1);
-    }
-}*/
-
-@Service
-class Listener {
-    @KafkaListener(topics = "quickstart-events1", groupId = "default", clientIdPrefix = "c1")
-    void listen(
-            @Payload String data,
-            @Header(KafkaHeaders.RECEIVED_PARTITION_ID) List<Integer> partitions,
-            @Header(KafkaHeaders.RECEIVED_TOPIC) List<String> topics,
-            @Header(KafkaHeaders.OFFSET) List<Long> offsets
-    ) {
-        System.out.println("=================");
-
-
-        System.out.format("partition: %s, topic: %s, offset: %s \n",
-                partitions,
-                topics,
-                offsets
-        );
-        System.out.println("data:" + data);
     }
 }
